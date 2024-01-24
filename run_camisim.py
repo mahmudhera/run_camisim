bdf6fbcf4a7b03176c5dfa8e27dbdd9c210a9843
Mahmudur <mahmudhera93@gmail.com>
2024-01-24 14:12:30 -0500

    added docum

--- run_camisim.py
+++ run_camisim.py
@@ -26,7 +26,21 @@ def get_fastq_filenames(outdir, simulation_directory_name, genome_name):
 
 def main():
     # parse arguments
-    parser = argparse.ArgumentParser(description='Process number of genomes.')
+    description = """
+    This script runs camisim with the given number of genomes, 
+    and then converts the bam files to ground truth files. Finally,
+    it merges the fastq files into a single file.
+    Inputs: 
+    - number of genomes to simulate (int)
+    - config file (will be written by this script, and used by camisim) (str)
+    - seed for simulations (int)
+    - output directory for camisim (str)
+    - size of the file in Gbp (float)
+    - gene ground truth filename (str)
+    - ko ground truth filename (str)
+    - merged metagenome filename (str)
+    Usage example: python run_camisim.py 10 --config config_seed_0_size_0.1.ini --seed 0 --outdir ./out_seed_0_size_0.1 --size 0.1 --gene_g_t gene_ground_truth_seed_0_size_0.1.csv --ko_g_t ko_ground_truth_seed_0_size_0.1.csv --metagenome_filename metagenome_seed_0_size_0.1.fastq"""
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('number_of_genomes', type=int, help='Number of genomes')
     parser.add_argument('--config', type=str, help='Config file (output of this script)', default='./config.ini')
     parser.add_argument('--seed', type=int, help='Seed used in simulation', default=0)
