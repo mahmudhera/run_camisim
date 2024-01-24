bc76860006ec9467370ef03d2fd107956f104067
Mahmudur <mahmudhera93@gmail.com>
2024-01-24 13:40:14 -0500

    use str

--- run_camisim.py
+++ run_camisim.py
@@ -242,7 +242,7 @@ def main():
             # open the gzipped file, read all the lines, and write to the outfile
             with gzip.open(fname, 'r') as infile:
                 for line in infile:
-                    outfile.write(line)
+                    outfile.write(str(line))
 
 if __name__ == '__main__':
     main()
