b8bbb05cfe181a615a4e4c69bf32e00913bec60e
Mahmudur <mahmudhera93@gmail.com>
2024-02-08 11:45:17 -0500

    use imports

--- compute_presence_absence_metrics.py
+++ compute_presence_absence_metrics.py
@@ -31,7 +31,7 @@ def main():
 
     # Read in the KO ground truth, headers: ko_id,abund_by_num_reads,abund_by_num_nts,abund_by_mean_cov,abund_by_med_cov
     ground_truth = pd.read_csv(args.ground_truth, sep=',')
-    ground_truth_kos = set(ground_truth['ko_id'])
+    ground_truth_kos_set = set(ground_truth['ko_id'])
     ground_truth_kos_list = ground_truth['ko_id'].tolist()
     ground_truth_abundances = ground_truth['abund_by_num_reads'].tolist()
 
@@ -39,7 +39,7 @@ def main():
     sorted_indices = np.argsort(ground_truth_abundances)[::-1]
     sorted_kos_ground_truth = [ground_truth_kos_list[i] for i in sorted_indices]
 
-    # Read in the KO predictions, ko_id,num_reads,num_nucleotides_covered,relative_abundance_by_num_reads,relative_abundance_by_nucleotides_covered
+    # Read in the KO predictions: ko_id,num_reads,num_nucleotides_covered,relative_abundance_by_num_reads,relative_abundance_by_nucleotides_covered
     predictions = pd.read_csv(args.predictions, sep=',')
     predicted_kos = predictions['ko_id'].tolist()
     if args.toolname == 'sourmash':
@@ -50,46 +50,41 @@ def main():
 
     # Sort the KO predictions by relative abundance (highest to lowest)
     sorted_indices = np.argsort(relative_abundances)[::-1]
-    sorted_kos = [predicted_kos[i] for i in sorted_indices]
-    sorted_relative_abundances = [relative_abundances[i] for i in sorted_indices]
+    sorted_kos_predictions = [predicted_kos[i] for i in sorted_indices]
+    predicted_kos_set = set(sorted_kos_predictions)
 
     # write headers to output file
     with open(args.output, 'w') as f:
-        f.write('purity,completeness\n')
+        f.write('purity,completeness,kendalltau\n')
 
-    # Compute precision and recall for each filter level
-    filter_levels = [1.0, 0.95, 0.9]
-    for filter_level in filter_levels:
-        # Filter the KO predictions so that the sum of rel abund of the filtered KOs make up top filter_level% of predictions
-        top_kos = set()
-        most_abundant_kos_list = []
-        top_rel_abund = 0.0
-        for ko, rel_abund in zip(sorted_kos, sorted_relative_abundances):
-            if top_rel_abund < filter_level:
-                top_kos.add(ko)
-                top_rel_abund += rel_abund
-                most_abundant_kos_list.append(ko)
-            else:
-                break
-        filtered_kos = top_kos
+    # Compute precision and recall
+    true_positives = len(ground_truth_kos_set.intersection(predicted_kos_set))
+    false_positives = len(predicted_kos_set) - true_positives
+    false_negatives = len(ground_truth_kos_set) - true_positives
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
 
-        # Compute precision and recall
-        true_positives = len(ground_truth_kos.intersection(filtered_kos))
-        false_positives = len(filtered_kos) - true_positives
-        false_negatives = len(ground_truth_kos) - true_positives
-        precision = true_positives / (true_positives + false_positives)
-        recall = true_positives / (true_positives + false_negatives)
+    # get the common kos between ground truth and predictions
+    common_kos = ground_truth_kos_set.intersection(predicted_kos_set)
 
-        # compute kendall tau using sorted_kos_ground_truth and most_abundant_kos_list
-        tau, p_value = kendalltau(sorted_kos_ground_truth, most_abundant_kos_list)
+    # get the sorted order of these commons kos in the ground truth
+    common_kos_sorted_indices = [sorted_kos_ground_truth.index(ko) for ko in common_kos]
+    common_kos_sorted_gt = [sorted_kos_ground_truth[i] for i in common_kos_sorted_indices]
 
-        # write as completeness and purity
-        completeness = recall
-        purity = precision
+    # get the sorted order of these commons kos in the predictions
+    common_kos_sorted_indices = [sorted_kos_predictions.index(ko) for ko in common_kos]
+    common_kos_sorted_pred = [sorted_kos_predictions[i] for i in common_kos_sorted_indices]
 
-        # Write precision, recall, and tau to output file
-        with open(args.output, 'a') as f:
-            f.write(f'{purity},{completeness},{tau}\n')
+    # compute kendall tau using sorted_kos_ground_truth and most_abundant_kos_list
+    tau, p_value = kendalltau(common_kos_sorted_gt, common_kos_sorted_pred)
+
+    # write as completeness and purity
+    completeness = recall
+    purity = precision
+
+    # Write precision, recall, and tau to output file
+    with open(args.output, 'a') as f:
+        f.write(f'{purity},{completeness},{tau}\n')
 
 if __name__ == '__main__':
     main()
