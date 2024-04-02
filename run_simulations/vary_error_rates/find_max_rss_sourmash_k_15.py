5a8eda63c8c37742add9e5680410f66a3fe803c2
Mahmudur <mahmudhera93@gmail.com>
2024-04-02 02:31:53 -0400

    fixed name conflict

--- run_simulations/vary_error_rates/find_max_rss_sourmash_k_15.py
+++ run_simulations/vary_error_rates/find_max_rss_sourmash_k_15.py
@@ -5,15 +5,14 @@ error_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
 seeds = range(2, 32)
 
 # for every error rate, find the maximum RSS value
-max_rss = {}
 for error_rate in error_rates:
-    max_rss[error_rate] = 0
+    max_rss = 0
     for seed in seeds:
         filename = f'sourmash_batch_output/metagenome_{error_rate}_seed_{seed}.fastq_resource_usage_k_15'
         with open(filename, 'r') as f:
             lines = f.readlines()
             for line in lines:
-                walltime, cputime, max_rss, avg_rss = line.strip().split(',')
-                max_rss[error_rate] = max(max_rss[error_rate], int(max_rss))
-
-print(max_rss)
+                walltime, cputime, max_rss_str, avg_rss = line.strip().split(',')
+                if float(max_rss_str) > max_rss:
+                    max_rss = float(max_rss)
+    print(f'Error rate: {error_rate}, max RSS: {max_rss}')
